0bbe1230e2ccaf4e9b642ddd2ea05c1d14882716
sumedha lakmal bandaranayaka <74480291+sumedhaLakmal@users.noreply.github.com>
2021-07-27 12:18:49 +0530

    Break even point

--- Mycode/Myex.java
+++ Mycode/Myex.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+//Header file
 import java.util.*;
 
 class Myex{
@@ -26,37 +26,6 @@ class Myex{
 	float BEP = fixed_c / contribution;
 	System.out.println("Break Even Point in product " + name +" : "+  BEP );
 	
-	}
-	
-=======
-import java.util.*;
+	}//end main function
 
-class Myex{
-	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-	
-	System.out.print("Enter Product Name : ");
-	String name = input.next();
-	System.out.println();
-
-	System.out.print("Enter product Selling Price :");
-	float selling_p = input.nextFloat();
-	
-	System.out.print("Enter product Variable Cost :");
-	float variable_c = input.nextFloat();
-	
-	System.out.print("Enter Fixed Cost :");
-	float fixed_c = input.nextFloat();
-	
-	System.out.println();
-	
-	float contribution = selling_p - variable_c;
-	System.out.println("Contribution per units :" + contribution );
-	
-	float BEP = fixed_c / contribution;
-	System.out.println("Break Even Point in product " + name +" : "+  BEP );
-	
-	}
-	
->>>>>>> dd92c9101436f0d21360b70a12ee63d094e69ae6
-}
+}//end class
