4fc83507c8d7e1386471912daecd1856ce3341d0
Sumedha Lakmal Bandaranayaka <74480291+sumedhaLakmal@users.noreply.github.com>
2021-07-28 00:10:23 +0530

    Java Methods and Classes

---
+++ Methods_Classes/Example.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Example{
+	public static void main (String []args){
+		myMethod();
+	}
+	public static void myMethod(){
+		System.out.println("MyMethod");
+	}
+	public static void myMethod(int x){
+		System.out.println("My Method(int)");
+	}
+	
+	public static void myMethod(int x,int y){
+		System.out.println("My Method(int)");
+	}
+	
+
+}
